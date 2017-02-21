10e8d84143ee70a1554c82ee854f5953f38d849f
c0demark <kumarvijay336@gmail.com>
2017-02-22 00:09:00 +0530

    boot-create-lb-tables modified'

--- server/boot/create-lb-tables.js
+++ server/boot/create-lb-tables.js
@@ -1,14 +1,14 @@
 'use strict';
+// var server = require('./server');
 
 module.exports = function(server) {
     // enable authentication
     //   server.enableAuth();
     var ds = server.dataSources.mysqlDs;
     var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
-    ds.automigrate(lbTables, function(er) {
-        if (er) throw er;
+    ds.automigrate(lbTables, function(err) {
+        if (err) throw err;
         console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
         ds.disconnect();
     });
 };
-// var server = require('./server');
